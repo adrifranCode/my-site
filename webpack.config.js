43981991358e1400b7725ce00d68e0fc16960f67
Adrien Francois <francois18.af@gmail.com>
2017-12-04 13:19:19 -0400

    Finished desktop and mobile header css

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+//  Babel and Webpack Configuration
+
+module.exports = {
+	entry: "./app/assets/scripts/App.js",
+	output:{
+		path: path.resolve(__dirname, "./app/temp/scripts" ),
+		filename:"App.js"
+	},
+	module:{
+		loaders:[
+			{
+				loader:'babel-loader',
+				query: {
+					presets:['es2015']
+				},
+				test:/\.js$/,
+				exclude:/node_modules/
+			}
+		]
+	}
+}
