bba14ee197abc4569f4cf5a6e04615226293f71d
Adrien Francois <francois18.af@gmail.com>
2017-12-04 14:55:21 -0400

    Completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,12 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 
+new RevealOnScroll($(".feature-item"), "85%");
 
+new RevealOnScroll($(".section-title"), "70%");
+
+new RevealOnScroll($(".testimonials"), "65%");
 
