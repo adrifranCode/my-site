5a8fe0ddaaf684439573491ec916903f6a852805
Adrien Francois <francois18.af@gmail.com>
2017-12-10 08:52:24 -0400

    Completed moverlay modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
@@ -7,4 +8,6 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".section-title"), "70%");
 
 new RevealOnScroll($(".testimonials"), "65%");
+
+var modal = new Modal();
 
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+
+class Modal {
+	constructor(){
+		this.openModalButton = $(".open-modal");
+		this.modal = $(".modal");
+		this.closeModalButton = $(".modal__close");
+		this.events();
+	}
+	events(){
+		this.openModalButton.click(this.openModal.bind(this));
+	
+		this.closeModalButton.click(this.closeModal.bind(this));
+	}
+	openModal(){
+		this.modal.addClass("modal--is-visible");
+		return false;
+	}
+	
+	closeModal(){
+		this.modal.removeClass("modal--is-visible");
+	}
+}
+
+export default Modal;
