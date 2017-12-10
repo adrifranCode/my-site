426efbd628ef8955ea20ecf6d3e5ed6450664aef
Adrien Francois <francois18.af@gmail.com>
2017-12-10 09:42:55 -0400

    added lazyloading and changed Modal.js to refresh all waypoints on load

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,9 +5,9 @@ import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 
-new RevealOnScroll($(".section-title"), "70%");
+new RevealOnScroll($(".section-title"), "84%");
 
-new RevealOnScroll($(".testimonials"), "65%");
+new RevealOnScroll($(".testimonials"), "83%");
 
 var modal = new Modal();
 
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class Modal {
 	constructor(){
+		this.lazyImages = $(".lazyload");
 		this.openModalButton = $(".open-modal");
 		this.modal = $(".modal");
 		this.closeModalButton = $(".modal__close");
 		this.events();
+		this.refreshWaypoints();
+	}
+	
+	refreshWaypoints() {
+		this.lazyImages.on('load', function() {
+			Waypoint.refreshAll();
+		});
 	}
 	events(){
 		this.openModalButton.click(this.openModal.bind(this));
